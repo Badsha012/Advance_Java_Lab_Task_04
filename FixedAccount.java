b40888264a939d013c802e3cb59415c268275e29
Badsha012 <badshasheikh361@gmail.com>
2026-03-10 11:23:26 +0600

    added a bank class

---
+++ FixedAccount.java
@@ -0,0 +1,12 @@
+public class FixedAccount extends Account {
+    private int tenureYear;
+
+    public FixedAccount() {}
+
+    public void setTenureYear(int tenureYear) {
+        this.tenureYear = tenureYear;
+    }
+    public int getTenureYear() {
+        return tenureYear;
+    }
+}
