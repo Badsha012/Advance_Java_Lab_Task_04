96e8e1a6566eecacf3c104560ff5dfbfc35bce0b
Badsha012 <badshasheikh361@gmail.com>
2026-03-10 11:16:31 +0600

    create  a java new project

---
+++ Account.java
@@ -0,0 +1,18 @@
+public class Account {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public Account() {}
+
+    public void setAccountNumber(String accountNumber) { this.accountNumber = accountNumber; }
+    public void setAccountHolderName(String accountHolderName) { this.accountHolderName = accountHolderName; }
+    public void setBalance(double balance) { this.balance = balance; }
+
+    public String getAccountNumber() { return accountNumber; }
+    public String getAccountHolderName() { return accountHolderName; }
+    public double getBalance() { return balance; }
+
+    public void depositMoney(double amount) { balance += amount; }
+    public void withdrawMoney(double amount) { balance -= amount; }
+}
